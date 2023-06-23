c227c9d25cce6ff8042aa44ec6d9b40c12529d80
Winnie Liu <pewinonlyy@gmail.com>
2023-06-22 22:25:15 -0700

    add forecast API

--- src/api/API.js
+++ src/api/API.js
@@ -1,11 +1,18 @@
 const API_KEY = `c072ebee80b5be5742303123edcbe14e`;
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=`
+const BASE_URL = `https://api.openweathermap.org/data/2.5/`
 
 
 
 export async function fetchWeather(city){
-    const response = await fetch(`${BASE_URL}${city}&appid=${API_KEY}`, {
+    const response = await fetch(`${BASE_URL}weather?q=${city}&appid=${API_KEY}`, {
       })
       const data = await response.json()
       return data
+}
+
+export async function fetchForecast(city){
+  const response = await fetch(`${BASE_URL}forecast?q=${city}&appid=${API_KEY}`, {
+    })
+    const data = await response.json()
+    return data
 }
